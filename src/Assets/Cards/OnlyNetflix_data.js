2dee70b1d707d6a7fd283f3826288fb9c3d33211
ManiKumar Nakka <manikumarnakka324@gmail.com>
2024-07-29 21:43:09 +0530

    updated-code

--- src/Assets/Cards/OnlyNetflix_data.js
+++ src/Assets/Cards/OnlyNetflix_data.js
@@ -1,6 +1,14 @@
 import maharaja from './maharaja.jpeg';
 import pathaan from './pathaan.jpg';
 import gadar from './gadar.jpeg';
+import vikram from'./vikram.jpg';
+import kushi from './kushi.jpg';
+import baby from './baby.jpg';
+import ismart from './ismart.jpeg';
+import balagam from './balagam.jpeg';
+import ganesha from './ganesha.jpeg';
+import dhrushyam from './dhrushyam.jpg';
+
 const OnlyNetflix_data=[
     {
         id:1,
@@ -11,32 +19,47 @@ const OnlyNetflix_data=[
         id:2,
         image:pathaan,
         name:"Pathaan"
-    }
-    ,{
+    },
+    {
         id:3,
+        image:vikram,
+        name:"VIKRAM"
+    },
+    {
+        id:4,
+        image:kushi,
+        name:"KUSHI"
+    },
+    {
+        id:5,
+        image:baby,
+        name:"BABY"
+    },
+    {
+        id:6,
+        image:ismart,
+        name:"ISMART"
+    },
+    {
+        id:7,
+        image:balagam,
+        name:"BALAGAM"
+    },
+    {
+        id:8,
         image:gadar,
         name:"Gadar"
     },
     {
-
+        id:9,
+        image:ganesha,
+        name:"GAM GAM GANESHA"
     },
     {
+        id:10,
+        image:dhrushyam,
+        name:"DHRUSHYAM"
 
-    },
-    {
-
-    },
-    {
-
-    },
-    {
-
-    },
-    {
-
-    },
-    {
-        
     }
 ]
 
